2add2d02c471c89a00f0298f7c3f240bcd0058f1
Romanovsk1 <93801993+Romanovsk1@users.noreply.github.com>
2022-01-25 20:15:51 +0400

    Добавьте файлы проекта.

---
+++ ClassLibrary1/ClassLibrary1/ClassLibrary1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library1
+{
+    public class ClassLibrary1
+    {
+        public static double Add(double firstValue, double secondValue) 
+        {
+          return firstValue + secondValue;
+        }
+
+        public static double Subtract(double firstValue, double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        public static double Multiply (double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+
+        public static double Divide(double firstValue, double secondValue)
+        {
+            if (secondValue == 0)
+                return 0;
+
+            return firstValue / secondValue;
+        }
+
+    }
+}
---
+++ DZ7.1/DZ7.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library1;
+
+namespace DZ7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = ClassLibrary1.Divide(5,2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
